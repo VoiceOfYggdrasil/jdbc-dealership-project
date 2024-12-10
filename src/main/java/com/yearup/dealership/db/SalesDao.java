7d298209610a228bbc2bc50d2ca6b5615bc7b220
VoiceOfYggdrasil <aaron.schmitt8@gmail.com>
2024-12-10 16:36:48 -0500

    Filled out SalesDAO and LeaseDAO

---
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -0,0 +1,33 @@
+package com.yearup.dealership.db;
+
+import com.yearup.dealership.models.SalesContract;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SalesDao {
+    private DataSource dataSource;
+
+    public SalesDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void addSalesContract(SalesContract salesContract) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO sales_contracts (vin, sale_date, monthly_payment) " +
+                             "VALUES(?,?,?) "
+             )) {
+            preparedStatement.setString(1, salesContract.getVin());
+            preparedStatement.setDate(2, Date.valueOf(salesContract.getSaleDate()));
+            preparedStatement.setDouble(4, salesContract.getPrice());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
